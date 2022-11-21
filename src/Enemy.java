c6849c4470c3d713ad15d01f3a73b07c105541f1
GabrielArdy <672021019@student.uksw.edu>
2022-11-21 18:01:00 +0700

    fixing enemy attack

--- src/Enemy.java
+++ src/Enemy.java
@@ -3,13 +3,13 @@ import java.lang.Math;
 public class Enemy {
     public int health;
     public int attack;
-    int min = 1;
-    int max = 10;
-    double range = Math.random() * (max - min + 1) + min;
+    int min = 5;
+    int max = 25;
+    int damage = (int) (Math.random() * (max - min + 1) + min);
 
     public Enemy() {
         this.health = 100;
-        this.attack = (int) Math.random() * 10 + min;
+        this.attack = damage;
     }
 
     public void attack(Player player) {
